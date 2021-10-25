2e6e3081b2ed9a44690eef142abae5886f197b74
liss22 <liss22@hanmail.net>
2021-10-25 14:06:57 +0900

    자이로 센서값 추가됨

--- Spatial.gd
+++ Spatial.gd
@@ -7,6 +7,10 @@ func _ready():
 
 func _process(delta):
 	var acc:= Input.get_accelerometer()
-	$CenterContainer/VBoxContainer/x.text = 'X: %s' % acc.x
-	$CenterContainer/VBoxContainer/y.text = 'Y: %s' % acc.y
-	$CenterContainer/VBoxContainer/z.text = 'Z: %s' % acc.z
+	$CenterContainer/hbox/acc/x.text = 'accX: %s' % acc.x
+	$CenterContainer/hbox/acc/y.text = 'accY: %s' % acc.y
+	$CenterContainer/hbox/acc/z.text = 'accZ: %s' % acc.z
+	var gyro:= Input.get_gyroscope()
+	$CenterContainer/hbox/gyro/x.text = 'gyroX: %s' % gyro.x
+	$CenterContainer/hbox/gyro/y.text = 'gyroY: %s' % gyro.y
+	$CenterContainer/hbox/gyro/z.text = 'gyroZ: %s' % gyro.z
